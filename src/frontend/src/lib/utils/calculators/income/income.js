27324d85b1a7de7aef768214c19796ffc616d647
moaiii <c.melville@hotmail.co.uk>
2019-03-07 23:00:42 +0000

    adding types jsdoc

--- src/frontend/src/lib/utils/calculators/income/income.js
+++ src/frontend/src/lib/utils/calculators/income/income.js
@@ -1,21 +1,37 @@
 import { groupByDate } from '../../dates';
 
+
+/**
+ * @param {Array<Object>} transactions
+ * @param {string} rangeType
+ * @returns {Array<Object>}
+ */
 const getIncome = (transactions, rangeType = 'month') => {
+  /** @type {Array<Object>} */
   const incomeTransactions = transactions
     .filter(transaction => transaction.category === 'Income'
       && parseInt(transaction.in, 10) > 0);
 
+  /** @type {Array<string: Object>} */
   const transactionsGroupedByMonth = groupByDate(incomeTransactions, rangeType, 'name');
 
-  const dateKeys = Object.keys(transactionsGroupedByMonth);
-  const incomeTransactionsArray = Object.values(transactionsGroupedByMonth);
+  /** @type {Array<string>} */
+  const dateKeys = Object
+    .keys(transactionsGroupedByMonth); // month or just number
 
+  /** @type {Array<Object>} */
+  const incomeTransactionsArray = Object
+    .values(transactionsGroupedByMonth);
+
+  /** @type {Array<number>} */
   const totalsByRange = incomeTransactionsArray
     .map((transactionGroup) => {
       return transactionGroup
         .reduce((acc, cur) => acc + parseInt(cur.in, 10), 0);
     });
 
+  /** @type {Object} finalObject.incomeTransactions */
+  /** @type {number} finalObject.total */
   const finalObject = {};
 
   dateKeys.forEach((key, index) => {
@@ -24,7 +40,7 @@ const getIncome = (transactions, rangeType = 'month') => {
       total: totalsByRange[index],
     };
   });
-  console.log(finalObject);
+
   return finalObject;
 };
 
