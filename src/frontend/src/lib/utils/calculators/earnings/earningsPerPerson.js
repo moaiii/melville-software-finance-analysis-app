95cde4f2ecf5d48bd30a5821fa7b75688ef75b10
chrismelville <chrismelville@skyscanner.net>
2019-03-07 16:00:52 +0000

    earnings per person

--- src/frontend/src/lib/utils/calculators/earnings/earningsPerPerson.js
+++ src/frontend/src/lib/utils/calculators/earnings/earningsPerPerson.js
@@ -1,10 +1,48 @@
 import store from '../../../../db/store';
 import people from '../../constants/people.json';
+import tax from '../tax';
 
 const earningsPerPerson = () => {
   const transactions = store.getState().TransactionListReducer.transactions.display;
 
-  return {};
+  const earningsPaymentsFromBusiness = transactions
+    .filter(transaction => transaction.category === 'dividend' || transaction.category === 'salary');
+
+  const earningsPerPerson = people
+    .map(person => {
+      const personalPayments = earningsPaymentsFromBusiness // salary and dividend
+        .filter(payment => payment.person === person);
+
+      const totalDividend = personalPayments
+        .filter(payment => payment.category === 'dividend')
+        .reduce((acc, cur) => acc + cur.value, 0);
+
+      const totalSalary = personalPayments
+        .filter(payment => payment.category === 'salary')
+        .reduce((acc, cur) => acc + cur.value, 0);
+
+      const grossEarnings = personalPayments
+        .reduce((acc, cur) => acc + cur.value, 0);
+
+      const _tax = tax.calculateTotalTax(grossEarnings, '2018-2019', 8424);
+      const netEarnings = grossEarnings - _tax.totalTaxAmount;
+
+      return {
+        dividend: totalDividend,
+        salary: totalSalary,
+        tax: _tax.totalTaxAmount,
+        grossEarnings,
+        netEarnings,
+      }
+    })
+
+  let obj = {};
+
+  Object
+    .keys(people)
+    .forEach((person, index) => obj[person] = earningsPerPerson[index]);
+
+  return obj;
 };
 
 export default {
